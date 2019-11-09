4384e19f8cdd24510238e7f880d9e6384e05fd0b
David Tennander <david.tennander@gmail.com>
2019-11-09 18:16:00 +0100

    Allows any host to call API.

--- back-end/src/main/scala/com/github/dtennander/todorpg/Backend.scala
+++ back-end/src/main/scala/com/github/dtennander/todorpg/Backend.scala
@@ -7,19 +7,16 @@ import com.github.dtennander.todorpg.endpoints.todos.TodosResource
 import com.github.dtennander.todorpg.endpoints.users.UsersResource
 import com.github.dtennander.todorpg.users.UserHandlerImpl
 import fs2.Stream
+import org.http4s.HttpRoutes
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.server.middleware.Logger
+import org.http4s.server.middleware.{CORS, Logger}
 
 object Backend extends IOApp {
 
   def stream[F[_]: ConcurrentEffect](implicit T: Timer[F], C: ContextShift[F]): Stream[F, Nothing] = {
       for {
-        httpApp <- Stream {(
-            new UsersResource[F]().routes(new UserHandlerImpl[F]()) <+>
-            new ListsResource[F]().routes(new lists.ListHandlerImpl[F]()) <+>
-            new TodosResource[F]().routes(new todos.TodosHandlerImpl[F]())
-          ).orNotFound}
+        httpApp <- Stream(addMiddleware(routes).orNotFound)
         finalHttpApp = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
         port <- Stream.eval(getPort)
         exitCode <- BlazeServerBuilder[F]
@@ -29,12 +26,21 @@ object Backend extends IOApp {
       } yield exitCode
     }.drain
 
-  def getPort[F[_]: Sync]: F[Int] = for {
-    port <- Sync[F].delay(Integer.valueOf(sys.env("PORT"))).attempt
-  } yield port match {
-    case Right(p) => p.toInt
-    case Left(_) => 8080
+  private def addMiddleware[F[_] : ConcurrentEffect](routes: HttpRoutes[F]) = {
+    CORS(routes)
   }
+
+  private def routes[F[_] : ConcurrentEffect]: HttpRoutes[F] =
+      new UsersResource[F]().routes(new UserHandlerImpl[F]()) <+>
+      new ListsResource[F]().routes(new lists.ListHandlerImpl[F]()) <+>
+      new TodosResource[F]().routes(new todos.TodosHandlerImpl[F]())
+
+  def getPort[F[_]: Sync]: F[Int] = for {
+      port <- Sync[F].delay(Integer.valueOf(sys.env("PORT"))).attempt
+    } yield port match {
+      case Right(p) => p.toInt
+      case Left(_) => 8080
+    }
 
   def run(args: List[String]): IO[ExitCode] = Backend.stream[IO].compile.drain.as(ExitCode.Success)
 }
