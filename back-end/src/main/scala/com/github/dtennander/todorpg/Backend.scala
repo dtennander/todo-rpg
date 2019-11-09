f253649a1a3b3d13659fa554af67656516413b54
David Tennander <david.tennander@gmail.com>
2019-11-09 17:48:04 +0100

    Adds default Port.

--- back-end/src/main/scala/com/github/dtennander/todorpg/Backend.scala
+++ back-end/src/main/scala/com/github/dtennander/todorpg/Backend.scala
@@ -2,12 +2,11 @@ package com.github.dtennander.todorpg
 
 import cats.effect._
 import cats.implicits._
-import com.github.dtennander.todorpg.endpoints.users.UsersResource
 import com.github.dtennander.todorpg.endpoints.lists.ListsResource
 import com.github.dtennander.todorpg.endpoints.todos.TodosResource
+import com.github.dtennander.todorpg.endpoints.users.UsersResource
 import com.github.dtennander.todorpg.users.UserHandlerImpl
 import fs2.Stream
-
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
@@ -22,10 +21,7 @@ object Backend extends IOApp {
             new TodosResource[F]().routes(new todos.TodosHandlerImpl[F]())
           ).orNotFound}
         finalHttpApp = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
-        port <- Stream {
-          val portStr = sys.env("PORT")
-          Integer.valueOf(portStr)
-        }
+        port <- Stream.eval(getPort)
         exitCode <- BlazeServerBuilder[F]
           .bindHttp(port, "0.0.0.0")
           .withHttpApp(finalHttpApp)
@@ -33,6 +29,12 @@ object Backend extends IOApp {
       } yield exitCode
     }.drain
 
+  def getPort[F[_]: Sync]: F[Int] = for {
+    port <- Sync[F].delay(Integer.valueOf(sys.env("PORT"))).attempt
+  } yield port match {
+    case Right(p) => p.toInt
+    case Left(_) => 8080
+  }
 
   def run(args: List[String]): IO[ExitCode] = Backend.stream[IO].compile.drain.as(ExitCode.Success)
 }
